bc5462b2541f280f639f22b19879aeedcdd54e61
claudio.dangelo <claudio.dangelo@softsolutions.it>
2020-05-19 07:57:18 +0200

    Corrected the limit parsing

--- core/src/main/java/org/pentaho/di/trans/steps/cassandrainput/CassandraInputMeta.java
+++ core/src/main/java/org/pentaho/di/trans/steps/cassandrainput/CassandraInputMeta.java
@@ -530,6 +530,7 @@ public class CassandraInputMeta extends BaseStepMeta implements StepMetaInterfac
       if ( subQ.toLowerCase().indexOf( "limit" ) > 0 ) { //$NON-NLS-1$
         String limitS =
           subQ.toLowerCase().substring( subQ.toLowerCase().indexOf( "limit" ) + 5, subQ.length() ).trim(); //$NON-NLS-1$
+        limitS = limitS.split(" ")[0];
         limitS = limitS.replaceAll( ";", "" ); //$NON-NLS-1$ //$NON-NLS-2$
         try {
           m_rowLimit = Integer.parseInt( limitS );
